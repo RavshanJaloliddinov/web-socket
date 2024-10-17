b11e0d3c4c77dd28d92e6323ef90296a92755a09
Ravshan <jaloliddinov008@gmail.com>
2024-10-17 21:17:25 +0500

    error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService)
+  const config = new DocumentBuilder()
+    .setTitle("microservice")
+    .setDescription('api')
+    .setVersion('1.0')
+    .addTag('mircoservice')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("api", app, document)
+
+  await app.listen(configService.get<number>('appConfig.port'), () => {
+    console.log(`Listening on ${configService.get<number>('appConfig.port')}`)
+  });
 }
 bootstrap();
