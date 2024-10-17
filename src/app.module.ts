c37d0ff84c20a351d21f8aa0c3e21b2be4cf4ec6
Ravshan <jaloliddinov008@gmail.com>
2024-10-17 20:09:57 +0500

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ClientModule } from './client/client.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, ClientModule],
+  imports: [UserModule, ClientModule, AuthModule],
   controllers: [],
   providers: [],
 })
