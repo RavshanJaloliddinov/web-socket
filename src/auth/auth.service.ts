c973105cf0ea83e965eecd4760d7f1ee0884669a
Nodirbek Savbatov Sherzod O'g'li <116478730+nodir0306@users.noreply.github.com>
2024-10-17 21:17:04 +0500

    ok

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { AuthLoginDto } from './dto/auth-login.dto';
 import { AuthRegisterDto } from './dto/auth-register.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from 'src/user/models';
 
 
 @Injectable()
 export class AuthService {
+  constructor(@InjectModel(User) private userModel: typeof User){}
   login(payload: AuthLoginDto) {
+    const {email,password} = payload
+    const foundedUser = this.userModel.findOne()
     return 'This action adds a new auth';
   }
 
