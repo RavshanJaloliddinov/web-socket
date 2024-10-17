d6995c8adee01499602c3e65a898a4e634a31fcb
Nodirbek Savbatov Sherzod O'g'li <116478730+nodir0306@users.noreply.github.com>
2024-10-17 21:28:31 +0500

    ok

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { AuthLoginDto } from './dto/auth-login.dto';
 import { AuthRegisterDto } from './dto/auth-register.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from 'src/user/models';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User) private userModel: typeof User){}
-  login(payload: AuthLoginDto) {
+  constructor(@InjectModel(User) private userModel: typeof User,private jwt: JwtService){}
+  async login(payload: AuthLoginDto): Promise<object> {
     const {email,password} = payload
-    const foundedUser = this.userModel.findOne()
-    return 'This action adds a new auth';
+    const foundedUser = await this.userModel.findOne({where: {email}})
+    if(!foundedUser){
+      throw new NotFoundException("User not found")
+    }
+    const accesToken = await this.jwt.signAsync(
+      {
+        id: foundedUser.id,
+        email: foundedUser.email
+      }
+    )
+
+    return {
+      token: accesToken,
+      email: email,
+      id: foundedUser.id,
+    };
   }
 
   register(payload: AuthRegisterDto) {
