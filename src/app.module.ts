79466b98c0a7292ba4415f797ebc9bb0b43473b1
Ravshan <jaloliddinov008@gmail.com>
2024-10-17 20:07:41 +0500

    user clinet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ClientModule],
   controllers: [],
   providers: [],
 })
